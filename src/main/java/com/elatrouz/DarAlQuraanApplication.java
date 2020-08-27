32e2a9e556d578c75315c68295e2da7bb0c7c533
ahmed elatrouz <aelatrouz@gmail.com>
2020-08-27 13:27:16 +0100

    initial commit

---
+++ src/main/java/com/elatrouz/DarAlQuraanApplication.java
@@ -0,0 +1,13 @@
+package com.elatrouz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarAlQuraanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarAlQuraanApplication.class, args);
+    }
+
+}
